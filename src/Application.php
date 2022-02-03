b19ca51cc8e91176fd64e8d26eee6bb39c31132d
roblesdavid99 <81788796+roblesdavid99@users.noreply.github.com>
2022-02-03 18:32:19 -0500

    Update Application.php

--- src/Application.php
+++ src/Application.php
@@ -59,12 +59,12 @@ class Application extends BaseApplication
          * Only try to load DebugKit in development mode
          * Debug Kit should not be installed on a production system
          */
-        if (Configure::read('debug')) {
-            $this->addPlugin('DebugKit');
-        }
+//         if (Configure::read('debug')) {
+//             $this->addPlugin('DebugKit');
+//         }
 
-        // Load more plugins here
-    }
+//         // Load more plugins here
+//     }
 
     /**
      * Setup the middleware queue your application will use.
